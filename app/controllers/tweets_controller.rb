30123c94b881258291693808b5cc6f9fa9362610
Guillaume Zeldine <g.zeldine@hotmail.fr>
2025-08-11 15:02:49 -0300

    Adding scope to retrieve tweets from a specific user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,9 @@
 class TweetsController < ApplicationController
   def index
+    if params[:user_id].present?
+      return render json: Tweet.by_user_id(params[:user_id])
+    end
+
     render json: Tweet.all
   end
 end
