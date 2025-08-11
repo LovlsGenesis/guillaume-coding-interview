78c5548d407104ab5c52af2dfdc7901f615e872b
Guillaume Zeldine <g.zeldine@hotmail.fr>
2025-08-11 15:09:23 -0300

    Paginating tweets result defined by page and per_page params

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,9 @@ class TweetsController < ApplicationController
       return render json: Tweet.by_user_id(params[:user_id])
     end
 
-    render json: Tweet.all
+    page = (params[:page] || 1).to_i
+    per_page = (params[:per_page] || 5).to_i
+
+    render json: Tweet.order(:created_at).limit(per_page).offset((page - 1) * per_page)
   end
 end
