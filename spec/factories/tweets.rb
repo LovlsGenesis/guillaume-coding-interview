e2714e558140ef1a64b0b2d1262e1e56fe5ccb4e
Guillaume Zeldine <g.zeldine@hotmail.fr>
2025-08-11 15:20:57 -0300

    Creating Tweet factory, adding specs for tweets index pagination

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :tweet do
-    
+    user {create(:user)}
+    body { "Lorem ipsum" }
   end
 end
--- spec/requests/tweet_request_spec.rb
+++ spec/requests/tweet_request_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Tweets", type: :request do
 
+  before do
+    create_list(:tweet, 10, user: create(:user))
+  end
+
+  describe "#index" do
+    let(:result) { JSON.parse(response.body) }
+
+  context "Pagination" do
+    it "should paginate all the tweets" do
+        per_page = 3
+        get tweets_path(per_page: per_page)
+
+        expect(result.size).to eq(per_page)
+
+      end
+    end
+  end
 end
